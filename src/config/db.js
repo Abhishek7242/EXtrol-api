0d1955ee0b49bc1d0dbbf9022f9d36fd870b315c
Abhishek Verma <vabhishak45@gmail.com>
2025-10-18 08:54:29 +0530

    bug fixes

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,8 @@ const mongoose = require('mongoose');
 async function connectDB(uri) {
     if (!uri) throw new Error('MONGODB_URI not provided');
 
-    await mongoose.connect(uri, {
-        family: 4,                // force IPv4
-        serverSelectionTimeoutMS: 10000
-    });
+   
+    await mongoose.connect(uri, { family: 4, serverSelectionTimeoutMS: 10000 });
 
     console.log('✅ Connected to MongoDB successfully');
 }
